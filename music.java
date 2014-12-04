acf72cf033680e03df084540296ff965c549d37c
JPisaBrony <jdpohl789@gmail.com>
2014-12-04 17:50:02 -0600

    added comments

--- music.java
+++ music.java
@@ -18,48 +18,72 @@ public class music extends MIDlet implements PlayerListener, CommandListener {
   boolean started = true, paused = false;
   int debug = 0;
   
+  // command buttons
   Command play_pause = new Command("Play/Pause", Command.ITEM, 1);
   Command next = new Command("Next", Command.ITEM, 1);
   
+  // when the app first starts up
   public void startApp() {
+      // boolean for when the app first starts up or resuming
+      // true initially, set to false to prevent it from running when resuming from paused
       if(started) {
+	  // form name
 	  form = new Form("JP's Music Player");
+	  // adds buttons
 	  form.addCommand(play_pause);
 	  form.addCommand(next);
+	  // makes commands work
 	  form.setCommandListener(this);
+	  // sets form to display
 	  Display.getDisplay(this).setCurrent(form);
+	  // sets to false so startup only runs once
 	  started = false;
 	  getFolder();
 	  mainLoop();
       }
   }
   
+  // adds file names from the folder to the array
   public void getFolder() {
 	try {
+		// gets the folder with the music in it
 		dir = (FileConnection) Connector.open("file:///MemoryCard/MUSIC/Pony/");
+		// gets a listing of the files
 		e = dir.list();
 		i = 0;
+		// itterates through the folder until the end
 		while(e.hasMoreElements()) {
+			// sets the name into the array
 			FileNameArray[i] = (String) e.nextElement();
 			i++;
 		}
+		// closes the resource or else there is a resource leak
 		dir.close();
 	} catch(Exception e) {
 		form.append("Error opening folder");
 	}
   }
   
+  // the main loop that is executed every time a new song is queued up
   public void mainLoop() {
 	try {
+		// generate a random number so that we can get different songs to play
 		j = r.nextInt(fileCount);
+		// gets the name from the array
 		fileName = FileNameArray[j];
+		// adds the name to the form so we know what song is playing
 		form.append("currently playing: " + fileName);
+		// gets the file connection to the song selected
 		fc = (FileConnection) Connector.open("file:///MemoryCard/MUSIC/Pony/" + fileName);
+		// creates a music player instance
 		player = Manager.createPlayer(fc.openInputStream(), "audio/mpeg");
 		debug++;
+		// starts the music player
 		player.start();
 		debug++;
+		// adds a listener
 		player.addPlayerListener(this);
+		// closes the resource or else there is a resource leak 
 		fc.close();
 		debug++;
 	} catch(Exception e) {
@@ -67,10 +91,16 @@ public class music extends MIDlet implements PlayerListener, CommandListener {
 	}
   }
   
+  // this method is ran every time their is an update with the music player
   public void playerUpdate(Player player, String event, Object data) {
+      // checks if the music has hit the end of the track
       if(event == PlayerListener.END_OF_MEDIA) {
 	  try {
+	    // closes the music player
+	    // if this isnt done, then there is a resource leak that crashes
+	    // the program and prevents new songs from being queued
 	    player.close();
+	    // reruns the main loop to get a new song
 	    mainLoop();
 	  } catch(Exception e) {
 	    form.append("Error in update");
@@ -78,24 +108,37 @@ public class music extends MIDlet implements PlayerListener, CommandListener {
       }
   }
   
+  // this method is run every time one of the command buttons is hit
   public void commandAction(Command c, Displayable d) {
+      // if the play_pause was hit
       if(c == play_pause) {
 	  try {
+	    // checks if the song was paused
 	    if(paused) {
+		// plays the song
 		player.start();
+		// sets paused to false so that it will stop it next time
 		paused = false;
 	    }
+	    // if the pause was playing
 	    else {
+		// stops the player
 		player.stop();
+		// sets paused to true so that it will play it next time
 		paused = true;
 	    }
 	  } catch(Exception e) {
 	    form.append("Could not play or pause");
 	  }
       }
+      // if the next button was hit
       if(c == next) {
 	  try {
+	    // closes the music player
+	    // if this isnt done, then there is a resource leak that crashes
+	    // the program and prevents new songs from being queued
 	    player.close();
+	    // reruns the main loop to get a new song
 	    mainLoop();
 	  } catch(Exception e) {
 	    form.append("Could not play next");
@@ -109,7 +152,7 @@ public class music extends MIDlet implements PlayerListener, CommandListener {
 	  total++;
       return total;
   }
-
+  
   public void pauseApp() {
       
   }
